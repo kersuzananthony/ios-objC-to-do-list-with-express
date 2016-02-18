9c170009da0bb7350ef07eeb76b4238d8e284eb1
Kersuzan <kersuzan.anthony@gmail.com>
2016-02-18 11:18:47 +0800

    Initial Commit

---
+++ myToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myToDoList
+//
+//  Created by Kersuzan on 18/02/16.
+//  Copyright © 2016 Kersuzan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myToDoList
+//
+//  Created by Kersuzan on 18/02/16.
+//  Copyright © 2016 Kersuzan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
